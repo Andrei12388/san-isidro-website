46c070cf7030e6058fc2c2929d217d8db215d771
andrei12388 <robertandreib.up@gmail.com>
2026-01-31 04:45:12 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "San Isidro Website",
+  title: "JCSGO: San Isidro Website",
   description: "A Site for Systematic Discipleship",
 };
 
